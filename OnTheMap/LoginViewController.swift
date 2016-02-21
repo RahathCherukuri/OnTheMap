f0a6cc1b3bead489414330ec3d9a09eba950f921
Rahath Cherukuri <rahath.cherukuri@gmail.com>
2016-02-20 22:47:41 -0500

    Initial Commit

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Rahath cherukuri on 2/20/16.
+//  Copyright © 2016 Rahath cherukuri. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButton(sender: AnyObject) {
+        
+    }
+    
+}
+
