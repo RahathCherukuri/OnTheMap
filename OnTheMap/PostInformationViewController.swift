bbf388e6def2d9e6a50e01169299bf9895a8d18e
Rahath Cherukuri <rahath.cherukuri@homes.com>
2016-02-29 13:44:18 -0500

    Added VC and storyboard for posting the information.

---
+++ OnTheMap/PostInformationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostInformationViewController.swift
+//  OnTheMap
+//
+//  Created by Rahath cherukuri on 2/29/16.
+//  Copyright © 2016 Rahath cherukuri. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PostInformationViewController: UIViewController {
+
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var bottomView: UIView!
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    @IBOutlet weak var linkTextField: UITextField!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func searchOnTheMap(sender: UIButton) {
+    }
+    
+    
+}
